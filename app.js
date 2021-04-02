f4b347982bd1038ea0931f4663bd604a9b35179d
yasutomog <yasutomo.goto@gmail.com>
2021-04-02 12:21:04 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const profile = {
+  name: 'soarflat',
+  sex: 'male',
+  location: 'Tokyo',
+};
+
+const hoge = (message) => {
+  console.log(message);
+};
+
+const fooBar = (a, b, c) => {
+  console.log(a);
+  console.log(b);
+  console.log(c);
+};
+
+fooBar(
+  111,
+  {
+    hoge: 'hoge!',
+  },
+  profile
+);
